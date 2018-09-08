5888f42fbc14aba03be18f24362b9df02ae43519
Eugene Yang <eugene87222@gmail.com>
2018-09-08 11:25:05 +0800

    update demo.py

--- demo.py
+++ demo.py
@@ -2,7 +2,8 @@ import pandas as pd
 import cv2
 
 description = pd.read_csv('dataset/class-descriptions.csv',
-                          names=['label', 'description'])
+                          names=['label', 'description'], skiprows=[0])
+
 bounding_boxes = pd.read_csv('dataset/train_bounding_boxes.csv',
                            names=['img_id', 'source', 'label', 'confidence', 'xmin', 'xmax', 'ymin', 'ymax',
                                   'isoccluded', 'istruncated', 'isgroupof', 'isdepiction', 'isinside'],
@@ -20,14 +21,12 @@ for row in bounding_boxes.itertuples():
     # cv2.imshow('origin_img', img)
     # cv2.waitKey(0)
 
+    print(label2feature.get(row.label, 'lable doesn\'t exist'))
     xmin, xmax = int(width*row.xmin), int(width*row.xmax)
     ymin, ymax = int(length*row.ymin), int(length*row.ymax)
     cropped_img = img[ymin:ymax, xmin:xmax]
     cv2.imshow('cropped_img', cropped_img)
     cv2.waitKey(0)
-
-    # Error occur: row.label doesn't exist 
-    print(label2feature[row.label])
     cv2.destroyAllWindows()
 
 print(len(label2feature))
